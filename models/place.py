58829492d1b0bfcad64c4432a036b67d65e9e0ed
root <besckinder@gmail.com>
2024-01-05 18:58:10 -0800

    New, better tables

--- models/place.py
+++ models/place.py
@@ -9,7 +9,8 @@ from sqlalchemy.orm import relationship
 
 if models.storage_t == 'db':
     place_amenity = Table('place_amenity', Base.metadata,
-                          Column('place_id', String(60),
+                          Column('place_id',
+                                 String(60, collation='latin1_swedish_ci'),
                                  ForeignKey('places.id', onupdate='CASCADE',
                                             ondelete='CASCADE'),
                                  primary_key=True),
@@ -26,7 +27,8 @@ class Place(BaseModel, Base):
         __tablename__ = 'places'
         city_id = Column(String(60, collation='latin1_swedish_ci'),
                          ForeignKey('cities.id'), nullable=False)
-        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+        user_id = Column(String(60, collation='latin1_swedish_ci'),
+                         ForeignKey('users.id'), nullable=False)
         name = Column(String(128), nullable=False)
         description = Column(String(1024), nullable=True)
         number_rooms = Column(Integer, nullable=False, default=0)
