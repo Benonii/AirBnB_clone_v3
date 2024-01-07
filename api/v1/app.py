8a52e281ab2b46956747b0bfb7334b715f5519d5
root <besckinder@gmail.com>
2024-01-07 13:36:38 -0800

    Pycodestyle

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,18 +3,19 @@
 '''Main app module'''
 
 from flask import Flask
-
-app = Flask(__name__)
-
 from models import storage
 from api.v1.views import app_views
 from os import getenv
 
+app = Flask(__name__)
+
 app.register_blueprint(app_views, url_prefix="/api/v1")
+
 
 @app.teardown_appcontext
 def teardown(exception):
     storage.close()
+
 
 host = getenv('HBNB_API_HOST')
 port = getenv('HBNB_API_PORT')
