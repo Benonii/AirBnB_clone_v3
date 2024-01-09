77886fba184f02e7abcb01c91e59114b9a8b2bcf
root <besckinder@gmail.com>
2024-01-09 11:18:44 -0800

    CORS

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,12 +3,13 @@
 '''Main app module'''
 
 from flask import Flask, jsonify
+from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
 from os import getenv
 
 app = Flask(__name__)
-
+CORS(app, resources={r"/api/*": {"origins": "http://0.0.0.0"})
 app.register_blueprint(app_views, url_prefix="/api/v1")
 
 
