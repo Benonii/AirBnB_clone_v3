4040a3272d8045795027b0e34248c60476232e96
root <besckinder@gmail.com>
2024-01-09 04:30:54 -0800

    Final commit on task 2

--- models/place.py
+++ models/place.py
@@ -14,7 +14,8 @@ if models.storage_t == 'db':
                                  ForeignKey('places.id', onupdate='CASCADE',
                                             ondelete='CASCADE'),
                                  primary_key=True),
-                          Column('amenity_id', String(60, collation='latin1_swedish_ci'),
+                          Column('amenity_id',
+                                 String(60, collation='latin1_swedish_ci'),
                                  ForeignKey('amenities.id', onupdate='CASCADE',
                                             ondelete='CASCADE'),
                                  primary_key=True))
